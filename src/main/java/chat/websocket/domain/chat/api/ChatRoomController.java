0aa24972e2519b182c62364ec4ab0e2b0bba94f5
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-03-06 22:54:05 +0900

    feat: 채팅방 컨트롤러 구현

---
+++ src/main/java/chat/websocket/domain/chat/api/ChatRoomController.java
@@ -0,0 +1,57 @@
+package chat.websocket.domain.chat.api;
+
+import chat.websocket.domain.chat.application.ChatRoomService;
+import chat.websocket.domain.chat.dto.ChatRoomGetDto;
+import chat.websocket.domain.chat.dto.ChatRoomWithMessageDto;
+import chat.websocket.domain.chat.entity.ChatRoom;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+
+    // 채팅방 생성 폼
+    @GetMapping("/rooms/new")
+    public String createRoomForm() {
+        return "chat/createRoom";
+    }
+
+    // 채팅방 전체 조회
+    @GetMapping("/rooms")
+    public String getRooms(Model model) {
+        List<ChatRoomGetDto> roomList = chatRoomService.getRoomList();
+        model.addAttribute("roomList", roomList);
+        return "chat/rooms";
+    }
+
+    // 채팅방 생성
+    @PostMapping("/rooms/new")
+    public String createRoom(@ModelAttribute(name = "name") String name, @AuthenticationPrincipal
+            UserDetails user) {
+        ChatRoom chatRoom = chatRoomService.addRoom(user.getUsername(), name);
+        return "redirect:/chat/rooms/" + chatRoom.getId();
+    }
+
+    // 특정 채팅방 입장
+    @GetMapping("/room/{roomId}")
+    public String enterRoom(@PathVariable Long roomId, Model model) {
+        ChatRoomWithMessageDto chatRoom = chatRoomService.getChatRoom(roomId);
+        model.addAttribute("chatRoom", chatRoom);
+        return "chat/chatroom";
+    }
+
+
+}
