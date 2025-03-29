5adaa57abf94b60abf4757d5f3e4e001a9e67253
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-03-29 19:54:42 +0900

    feat: 채팅방 api 추가

---
+++ src/main/java/chat/websocket/domain/chat/api/ChatRoomApiController.java
@@ -0,0 +1,44 @@
+package chat.websocket.domain.chat.api;
+
+import chat.websocket.domain.chat.application.ChatRoomService;
+import chat.websocket.domain.chat.dto.ChatRoomGetDto;
+import chat.websocket.domain.chat.dto.ChatRoomWithMessageDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/chatRoom")
+@RequiredArgsConstructor
+public class ChatRoomApiController {
+
+    private final ChatRoomService chatRoomService;
+
+    @GetMapping
+    public ResponseEntity<List<ChatRoomGetDto>> getRooms() {
+        List<ChatRoomGetDto> roomList = chatRoomService.getRoomList();
+        return ResponseEntity.ok(roomList);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addRoom(@RequestBody String name, @AuthenticationPrincipal
+            UserDetails user) {
+        chatRoomService.addRoom(user.getUsername(), name);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{roomId}")
+    public ResponseEntity<ChatRoomWithMessageDto> getRoom(@PathVariable Long roomId) {
+        ChatRoomWithMessageDto chatRoom = chatRoomService.getChatRoom(roomId);
+        return ResponseEntity.ok(chatRoom);
+    }
+
+
+
+}
